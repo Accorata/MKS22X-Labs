6c9197759eeee747fe0514f1b9ada373ea2486ab
Henry Bach <henrypbach@gmail.com>
2022-02-03 10:51:25 -0500

    with testing

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -3,6 +3,7 @@ public class Recursion {
     //int n = Integer.parseInt(args[0]);
     //System.out.println();
     //printAllWords(n);
+    printNoDoubleLetterWords(3, new char[]{'a','b','c'});
   }
   public static void printAllWords(int length){
     printAllWords(length,"");
@@ -16,29 +17,15 @@ public class Recursion {
       }
     }
   }
-  /*Print all words that are made of the characters in the array of letters.
-  *There may not be consecutive equal letters, so:
-  *aax is not allowed, but axa is allowed.
-  *@param length : the length of the words that are to be printed
-          *@param letters: the letters you should be using,
-          *@precondition: letters contains at least 2 characters, and has no duplicates.
-          */
   public static void printNoDoubleLetterWords(int length,char[] letters){
     printNoDoubleLetterWords(length,"",letters);
   }
-
-          /*Print all words that are made of the characters in letters. There may not be consecutive equal letters,
-          *aax is not allowed, but axa is allowed.
-          *@param length : either how many more letters need to be
-          *@param word   : the partial word so far.
-          *@param letters: the letters you should be using
-          */
   public static void printNoDoubleLetterWords(int length,String word,char[]letters){
     if (word.length() >= length){
       System.out.println(word);
     } else {
       for (int i = 0; i<letters.length; i++){
-        if (word.lastIndexOf(letters[i]) != words.length()-1){
+        if (word.length() == 0 || word.lastIndexOf(letters[i]) != word.length()-1){
           printNoDoubleLetterWords(length, word+letters[i], letters);
         }
       }
