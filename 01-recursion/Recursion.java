775171f26e7d4104482d8a929dde4dfe79de406a
Henry Bach <henrypbach@gmail.com>
2022-02-03 10:40:34 -0500

    added tester

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -1,4 +1,9 @@
 public class Recursion {
+  public static void main(String[] args){
+    int n = Integer.parseInt(args[0]);
+    //System.out.println();
+    printAllWords(n);
+  }
   public static void printAllWords(int length){
     printAllWords(length,"");
   }
