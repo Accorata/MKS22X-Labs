03adf3b1d46fbc159085bfc617e9d15ca7f02dbb
Henry Bach <henrypbach@gmail.com>
2022-02-14 10:29:47 -0500

    base queen start

---
+++ 02-queens/QueenBoard.java
@@ -0,0 +1,12 @@
+public class QueenBoard {
+  private int[][]board;
+
+  private boolean addQueen(int r, int c){
+    if (board[r][c] == -1) return false;
+
+  }
+
+  private void removeQueen(int r, int c){
+    return true;
+  }
+}
