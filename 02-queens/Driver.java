44e34d7eda49d8a762ab2f528478bb921e4bea3c
Henry Bach <henrypbach@gmail.com>
2022-02-14 10:45:59 -0500

    toString + tests

--- 02-queens/Driver.java
+++ 02-queens/Driver.java
@@ -2,5 +2,6 @@ public class Driver {
   public static void main (String[] args){
     QueenBoard a = new QueenBoard(5);
     System.out.println(a.toStringDebug());
+    System.out.println(a);
   }
 }
--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -25,10 +25,21 @@ public class QueenBoard {
     board = new int [size][size];
     addQueen(1,1);
     addQueen(2,3);
-    removeQueen(1,1);
-    removeQueen(2,3);
+    //removeQueen(1,1);
+    //removeQueen(2,3);
   } //: initialize a square 2d array of the specified size. All values should be 0
-  //public String toString(){} //: see format in the comments below.
+  public String toString(){
+    String ans = "";
+    for (int i = 0; i<board.length; i++){
+      for (int j = 0; j<board.length; j++){
+        if (board[i][j] == -1) ans+="Q";
+        else ans+="_";
+        if (j != board.length-1) ans+=" ";
+      }
+      if (i != board.length-1) ans += "\n";
+    }
+    return ans;
+  } //: see format in the comments below.
   public String toStringDebug(){
     String ans = "";
     for (int i = 0; i<board.length; i++){
