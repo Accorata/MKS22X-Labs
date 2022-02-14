55a914de7b082d8954238cf2e7608848b8848eb2
Henry Bach <henrypbach@gmail.com>
2022-02-14 10:37:49 -0500

    add/remove

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -2,11 +2,31 @@ public class QueenBoard {
   private int[][]board;
 
   private boolean addQueen(int r, int c){
-    if (board[r][c] == -1) return false;
-
+    if (board[r][c] != 0) return false;
+    board[r][c] = -1;
+    for (int i = 1; i<board.length-r; i++){
+      if (c-i>0) board[r+i][c-i] += 1;
+      board[r+i][c] += 1;
+      if (c+i<board.length) board[r+i][c+i] += 1;
+    }
+    return true;
   }
 
   private void removeQueen(int r, int c){
+    board[r][c] = 0;
+    for (int i = 1; i<board.length-r; i++){
+      if (c-i>0) board[r+i][c-i] -= 1;
+      board[r+i][c] -= 1;
+      if (c+i<board.length) board[r+i][c+i] -= 1;
+    }
     return true;
   }
+
+  public QueenBoard(int size){
+    board = new int [size][size];
+  } //: initialize a square 2d array of the specified size. All values should be 0
+  public String toString(){} //: see format in the comments below.
+  public String toStringDebug(){}
+  public boolean solve(){} //note: wrapper method
+  public int countSolutions(){} //note: wrapper method
 }
