297728df54dcbf696bb2b151b2f35c1da002531c
Henry Bach <henrypbach@gmail.com>
2022-02-17 10:27:33 -0500

    +tests

--- 02-queens/Driver.java
+++ 02-queens/Driver.java
@@ -2,7 +2,8 @@ public class Driver {
   public static void main (String[] args){
     int n = Integer.parseInt(args[0]);
     QueenBoard a = new QueenBoard(n);
-    a.solve();
+    //a.solve();
     System.out.println(a.countSolutions());
+    System.out.println(a);
   }
 }
