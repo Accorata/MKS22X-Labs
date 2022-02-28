f28a9b30f684cf6c2d87ec963c61f120684a0e9e
Henry Bach <henrypbach@gmail.com>
2022-02-28 10:53:59 -0500

    toString and given methods

--- 03-maze/Maze.java
+++ 03-maze/Maze.java
@@ -14,5 +14,29 @@ public class Maze{
       System.exit(1);
     }
   }
-
+  private void wait(int millis){
+    try {
+      Thread.sleep(millis);
+    } catch (InterruptedException e) {
+    }
+  }
+  public void setAnimate(boolean b){
+    animate = b;
+  }
+  public static void clearTerminal(){
+    System.out.println("\033[2J");
+  }
+  public static void gotoTop(){
+    System.out.println("\033[1;1H");
+  }
+  public String toString(){
+    String ans = "";
+    for(int i = 0; i<data.length; i++){
+      for(int j = 0; j<data[0].length; j++){
+        ans+=data[i][j];
+      }
+      if (i != data.length-1) ans += "/n";
+    }
+    return ans;
+  }
 }
