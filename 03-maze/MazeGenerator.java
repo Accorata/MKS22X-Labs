e7bfeb1260c03c4ec22612c03cda6e8320ab6b63
Henry Bach <henrypbach@gmail.com>
2022-03-06 23:21:24 -0500

    complete (a lot of tests)

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -3,14 +3,27 @@ public class MazeGenerator {
   public static void generate(char[][]maze, int row, int col){
     generateR(maze, row, col);
     int endR = 1;
-    if (row < (maze.length-2)/2) endR = maze.length-2;
+    if (row <= (maze.length-2)/2) endR = maze.length-2;
     int endC = 1;
-    if (col < (maze[0].length-2)/2) endC = maze[0].length-2;
+    if (col <= (maze[0].length-2)/2) endC = maze[0].length-2;
     maze[row][col] = 'S';
     maze[endR][endC] = 'E';
+    if (!solve(maze, row, col)) {
+      for (int i = 0; i<maze.length; i++){
+        for (int j = 0; j<maze[0].length; j++){
+          maze[i][j] = '#';
+        }
+      }
+      generate(maze,row,col);
+    }
+    for (int i = 0; i<maze.length; i++){
+      for (int j = 0; j<maze[0].length; j++){
+        if (maze[i][j] == '.' || maze[i][j] == '@') maze[i][j] = ' ';
+      }
+    }
   }
   public static void generateR(char[][]maze, int row, int col){
-    if (row != 0 && row != maze.length-1 && col != 0 && col != maze[0].length-1) {
+    if (row != 0 && row != maze.length-1 && col != 0 && col != maze[0].length-1 && maze[row][col] != ' ') {
       int count = 0;
       if (maze[row-1][col] == ' ') count++;
       if (maze[row+1][col] == ' ') count++;
@@ -51,7 +64,7 @@ public class MazeGenerator {
     if (ans) return ans;
     ans = solve(maze, row,col+1);
     if (ans) return ans;
-    maze[row][col] = '.';
+    if (maze[row][col] != 'S') maze[row][col] = '.';
     return false;
   }
   //private static int[] pathDist(char[][] maze, int row, int col, int dis){
