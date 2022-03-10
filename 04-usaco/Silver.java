48e4de39490e46108a1c073cd8eff28677934cb3
Henry Bach <henrypbach@gmail.com>
2022-03-10 10:59:31 -0500

    should include ways generation

--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -15,12 +15,26 @@ public class Silver {
           space[i][j] = data.nextChar();
         }
       }
-      int[][] ways = generateWays(space, data.nextInt(), data.nextInt(), time, new int[spaceX][spaceY]);
+      int[][] ways = new int[spaceX][spaceY]);
+      ways[data.nextInt()][data.nextInt()] = 1;
+      ways = generateWays(space, time, ways)
     } catch (FileNotFoundException e){
       e.printStackTrace();
     }
   }
-  public static int[][] generateWays (char[][] space, int row, int col, int time, int[][] ans){
-    if (time == 0) return ans;
+  public static int[][] generateWays (char[][] space, int time, int[][] ways){
+    if (time == 0) return ways;
+    int[][] ans = new int[ways.length][ways[0].length];
+    for (int i = 0; i<spaceX; i++) {
+      for (int j = 0; j<spaceY; j++) {
+        if (space.getChar(i,j) != '*') {
+          ans += ways[i-1][j];
+          ans += ways[i+1][j];
+          ans += ways[i][j-1];
+          ans += ways[i][j+1];
+        }
+      }
+    }
+    return generateWays (space, time-1, ans);
   }
 }
