c42791b9aaa556eedc996ef4be76b38cb36b16ed
Henry Bach <henrypbach@gmail.com>
2022-03-11 11:00:36 -0500

    should work but doesn't

--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -8,46 +8,62 @@ public class Silver {
       Scanner data = new Scanner(f);
       int spaceX = data.nextInt();
       int spaceY = data.nextInt();
-      String[][] space = new String[spaceX][spaceY];
-      int time = data.nextInt();
-      for (int i = 0; i<spaceX; i++) {
-        //for (int j = 0; j<spaceY; j++) {
-          String ln = data.next();
-          //space[i][j] = data.next();
-          space[i] = ln.split(" ");
-        //}
-      }
       long[][] ways = new long[spaceX][spaceY];
+      int time = data.nextInt();
+      data.nextLine();
+      for (int i = 0; i<spaceX; i++) {
+        String[] ln = data.nextLine().split(" ");
+        for (int j = 0; j<spaceY; j++) {
+          if (ln[j].equals("*")) ways[i][j] = -1;
+        }
+      }
       ways[data.nextInt()-1][data.nextInt()-1] = 1;
-      ways = generateWays(space, time, ways);
+      //printAry(space);
+      printAry(ways);
+      ways = generateWays(spaceX, spaceY, time, ways);
       // System.out.println(data.nextInt());
       // System.out.println(data.nextInt());
+      printAry(ways);
       System.out.println(ways[data.nextInt()-1][data.nextInt()-1]);
     } catch (FileNotFoundException e){
       e.printStackTrace();
     }
   }
-  public static long[][] generateWays (String[][] space, int time, long[][] ways){
+  public static long[][] generateWays (int spaceX, int spaceY, int time, long[][] ways){
     if (time == 0) return ways;
     long[][] ans = new long[ways.length][ways[0].length];
-    for (int i = 0; i<space.length; i++) {
-      for (int j = 0; j<space[0].length; j++) {
-        if (space[i][j] != "*") {
-          if (isLegal(space, i-1, j)) ans[i][j] += ways[i-1][j];
-          if (isLegal(space, i+1, j)) ans[i][j] += ways[i+1][j];
-          if (isLegal(space, i, j-1)) ans[i][j] += ways[i][j-1];
-          if (isLegal(space, i, j+1)) ans[i][j] += ways[i][j+1];
+    for (int i = 0; i<spaceX; i++) {
+      for (int j = 0; j<spaceY; j++) { //space[0].length
+        if (ways[i][j] != -1) {
+          if (isLegal(spaceX, spaceY, i-1, j)) ans[i][j] += ways[i-1][j];
+          if (isLegal(spaceX, spaceY, i+1, j)) ans[i][j] += ways[i+1][j];
+          if (isLegal(spaceX, spaceY, i, j-1)) ans[i][j] += ways[i][j-1];
+          if (isLegal(spaceX, spaceY, i, j+1)) ans[i][j] += ways[i][j+1];
         }
       }
     }
-    return generateWays (space, time-1, ans);
+    printAry(ans);
+    return generateWays (spaceX, spaceY, time-1, ans);
   }
-  public static boolean isLegal (String[][] space, int r, int c){
-    if (r < 0 || r >= space.length) return false;
-    if (c < 0 || c >= space[0].length) return false;
+  public static boolean isLegal (int spaceX, int spaceY, int r, int c){
+    if (r < 0 || r >= spaceX) return false;
+    if (c < 0 || c >= spaceY) return false;
     return true;
   }
-  // public static String board (long[][] ways) {
-  //
-  // }
+  public static void printAry (long[][] ary) {
+    for (long[] row : ary) {
+      for (long i : row) {
+        System.out.print(i);
+      }
+      System.out.println();
+    }
+  }
+  public static void printAry (String[][] ary) {
+    for (String[] row : ary) {
+      for (String i : row) {
+        System.out.print(i);
+      }
+      System.out.println();
+    }
+  }
 }
