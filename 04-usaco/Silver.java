26998c4bc8df1e0061e117732bfc328244aad100
Henry Bach <henrypbach@gmail.com>
2022-03-11 10:41:25 -0500

    silver but always 0

--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -18,9 +18,11 @@ public class Silver {
         //}
       }
       long[][] ways = new long[spaceX][spaceY];
-      ways[data.nextInt()][data.nextInt()] = 1;
+      ways[data.nextInt()-1][data.nextInt()-1] = 1;
       ways = generateWays(space, time, ways);
-      System.out.println(ways[data.nextInt()][data.nextInt()]);
+      // System.out.println(data.nextInt());
+      // System.out.println(data.nextInt());
+      System.out.println(ways[data.nextInt()-1][data.nextInt()-1]);
     } catch (FileNotFoundException e){
       e.printStackTrace();
     }
@@ -31,13 +33,21 @@ public class Silver {
     for (int i = 0; i<space.length; i++) {
       for (int j = 0; j<space[0].length; j++) {
         if (space[i][j] != "*") {
-          ans[i][j] += ways[i-1][j];
-          ans[i][j] += ways[i+1][j];
-          ans[i][j] += ways[i][j-1];
-          ans[i][j] += ways[i][j+1];
+          if (isLegal(space, i-1, j)) ans[i][j] += ways[i-1][j];
+          if (isLegal(space, i+1, j)) ans[i][j] += ways[i+1][j];
+          if (isLegal(space, i, j-1)) ans[i][j] += ways[i][j-1];
+          if (isLegal(space, i, j+1)) ans[i][j] += ways[i][j+1];
         }
       }
     }
     return generateWays (space, time-1, ans);
   }
+  public static boolean isLegal (String[][] space, int r, int c){
+    if (r < 0 || r >= space.length) return false;
+    if (c < 0 || c >= space[0].length) return false;
+    return true;
+  }
+  // public static String board (long[][] ways) {
+  //
+  // }
 }
