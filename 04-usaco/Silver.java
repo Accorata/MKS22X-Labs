ea388d5c9a836021ddf75712f8252c07274e7897
Henry Bach <henrypbach@gmail.com>
2022-03-12 11:22:25 -0500

    working Silver (tested)

--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -12,18 +12,16 @@ public class Silver {
       int time = data.nextInt();
       data.nextLine();
       for (int i = 0; i<spaceX; i++) {
-        String[] ln = data.nextLine().split(" ");
-        for (int j = 0; j<spaceY; j++) {
-          if (ln[j].equals("*")) ways[i][j] = -1;
+        String ln = data.nextLine();
+        System.out.println(ln);
+        for (int j = 0; j<ln.length(); j++) {
+          if (ln.charAt(j) == '*') ways[i][j] = -1;
         }
       }
       ways[data.nextInt()-1][data.nextInt()-1] = 1;
-      //printAry(space);
-      printAry(ways);
+      //printAry(ways);
       ways = generateWays(spaceX, spaceY, time, ways);
-      // System.out.println(data.nextInt());
-      // System.out.println(data.nextInt());
-      printAry(ways);
+      //printAry(ways);
       System.out.println(ways[data.nextInt()-1][data.nextInt()-1]);
     } catch (FileNotFoundException e){
       e.printStackTrace();
@@ -35,32 +33,27 @@ public class Silver {
     for (int i = 0; i<spaceX; i++) {
       for (int j = 0; j<spaceY; j++) { //space[0].length
         if (ways[i][j] != -1) {
-          if (isLegal(spaceX, spaceY, i-1, j)) ans[i][j] += ways[i-1][j];
-          if (isLegal(spaceX, spaceY, i+1, j)) ans[i][j] += ways[i+1][j];
-          if (isLegal(spaceX, spaceY, i, j-1)) ans[i][j] += ways[i][j-1];
-          if (isLegal(spaceX, spaceY, i, j+1)) ans[i][j] += ways[i][j+1];
+          if (isLegal(spaceX, spaceY, i-1, j, ways)) ans[i][j] += ways[i-1][j];
+          if (isLegal(spaceX, spaceY, i+1, j, ways)) ans[i][j] += ways[i+1][j];
+          if (isLegal(spaceX, spaceY, i, j-1, ways)) ans[i][j] += ways[i][j-1];
+          if (isLegal(spaceX, spaceY, i, j+1, ways)) ans[i][j] += ways[i][j+1];
+        } else {
+          ans[i][j] = -1;
         }
       }
     }
-    printAry(ans);
+    //printAry(ans);
     return generateWays (spaceX, spaceY, time-1, ans);
   }
-  public static boolean isLegal (int spaceX, int spaceY, int r, int c){
+  public static boolean isLegal (int spaceX, int spaceY, int r, int c, long[][] ways){
     if (r < 0 || r >= spaceX) return false;
     if (c < 0 || c >= spaceY) return false;
+    if(ways[r][c] == -1) return false;
     return true;
   }
   public static void printAry (long[][] ary) {
     for (long[] row : ary) {
       for (long i : row) {
-        System.out.print(i);
-      }
-      System.out.println();
-    }
-  }
-  public static void printAry (String[][] ary) {
-    for (String[] row : ary) {
-      for (String i : row) {
         System.out.print(i);
       }
       System.out.println();
