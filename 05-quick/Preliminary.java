0fb8e09040d6d8b7d6a28798d918b74e0ae1ed48
Henry Bach <henrypbach@gmail.com>
2022-03-15 10:56:08 -0400

    with tests (Everything works!)

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -2,13 +2,13 @@ import java.util.Arrays;
 public class Preliminary {
   public static void main(String[] args) {
     int[] test = new int []{999,999,999,4,3,2,1,0,999,999,999};
-    int[] tests = new int []{7, 89, 3, 0, -4, 45, 89};
+    int[] ary = {2, 10, 15, 23, 0, 5};
+    int[] problem = {4,4,4,4,4};
     //System.out.println(partition(test,3,7));
-    System.out.println(quickselect(tests, Integer.parseInt(args[0])));
-    System.out.println(Arrays.toString(tests));
+    System.out.println(quickselect(problem, Integer.parseInt(args[0])));
+    System.out.println(Arrays.toString(problem));
   }
   public static int quickselect(int []data, int k){
-    k--;
     int start = 0;
     int end = data.length-1;
     int pivot = partition(data, start, end);
@@ -27,23 +27,24 @@ public class Preliminary {
     int p = (int)(Math.random()*(end-start+1) + start);
     int temp = data[start];
     boolean addFront = false;
+    if (end == data.length-1) addFront = true;
     data[start] = data[p];
     data[p] = temp;
     p = start;
     while(p < end) {
       if (data[p+1] == data[p]){
         if (addFront) {
+          addFront = false;
           temp = data[p+1];
           data[p+1] = data[p];
           data[p] = temp;
           p++;
-          addFront = false;
         } else {
+          addFront = true;
           temp = data[p+1];
           data[p+1] = data[end];
           data[end] = temp;
           end--;
-          addFront = true;
         }
       } else if (data[p+1] < data[p]) {
         temp = data[p+1];
