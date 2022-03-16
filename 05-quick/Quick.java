2703870c2bfb7aa5d063de9eb59f83a877ecb2d1
Henry Bach <henrypbach@gmail.com>
2022-03-16 10:22:20 -0400

    quicksort lst

--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -1,18 +1,24 @@
 import java.util.Arrays;
-public class Preliminary {
+public class Quick {
   public static void main(String[] args) {
     int[] test = new int []{999,999,999,4,3,2,1,0,999,999,999};
     int[] ary = {2, 10, 15, 23, 0, 5};
     int[] problem = {4,4,4,4,4};
     //System.out.println(partition(test,3,7));
-    System.out.println(quickselect(problem, Integer.parseInt(args[0])));
-    System.out.println(Arrays.toString(problem));
+    //System.out.println(quickselect(problem, Integer.parseInt(args[0])));
+    System.out.println(Arrays.toString(ary));
+    quicksort(ary);
+    System.out.println(Arrays.toString(ary));
   }
-  public static void quicksort(int[]data) {
-
+  public static void quicksort(int[] data) {
+    quicksort (data, 0, data.length-1);
   }
-  public static void quicksort ( int [] data, int start, int end){
-
+  public static void quicksort (int[] data, int start, int end){
+    if (start < end) {
+      int pivot = partition(data, start, end);
+      quicksort(data, start, pivot-1);
+      quicksort(data, pivot+1, end);
+    }
   }
   public static int quickselect(int []data, int k){
     int start = 0;
