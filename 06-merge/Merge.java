2a502823ab8155ba7a5c867f401d72d1a92a643f
Henry Bach <henrypbach@gmail.com>
2022-03-22 10:50:17 -0400

    mergesort lst (removed prints, transfered helper, +tests)

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -5,8 +5,9 @@ public class Merge {
     int[] b = new int[]{-3,-1, 0, 0, 5, 6};
     int[] n = merge(a,b);
     int[] arr = new int[]{5,67, 8,-1, 6, -89, 0, 0, 0, 0, 1, 1};
-    int[] m = mergesortH(arr);
-    System.out.println(Arrays.toString(m));
+    //int[] m = mergesortH(arr);
+    mergesort(arr);
+    System.out.println(Arrays.toString(arr));
   }
   public static int [] merge(int [] left, int[] right){
     int[] newArr = new int[left.length+right.length];
@@ -23,12 +24,12 @@ public class Merge {
     }
     return newArr;
   }
-
-  // public static void mergesort(data){
-  //   int [] temp  =  mergeSortH(data);
-  //   //copy temp into data
-  // }
-
+  public static void mergesort(int[] data){
+    int [] temp  =  mergesortH(data);
+    for (int i = 0; i<data.length; i++){
+      data[i] = temp[i];
+    }
+  }
   public static int[] mergesortH(int[] data){
     if (data.length < 2) return data;
     int[] left = new int[data.length/2];
@@ -39,7 +40,7 @@ public class Merge {
     for (int i = 0; i<right.length; i++){
       right[i] = data[i+data.length/2];
     }
-    System.out.println(Arrays.toString(data)+"  "+Arrays.toString(left)+"  "+Arrays.toString(right));
+    //System.out.println(Arrays.toString(data)+"  "+Arrays.toString(left)+"  "+Arrays.toString(right));
     return merge(mergesortH(left),mergesortH(right));
   }
 }
