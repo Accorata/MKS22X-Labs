418e8078d8bc1feca1bfd4c4ead79597ad24e1f7
Henry Bach <henrypbach@gmail.com>
2022-03-22 10:34:09 -0400

    merge 1st

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -1,31 +1,47 @@
+import java.util.Arrays;
 public class Merge {
+  public static void main(String[] args) {
+    int[] a = new int[]{3,4,4,5,0,6};
+    int[] b = new int[]{-3, 6, -1, 0, 0, 5};
+    int[] n = merge(a,b);
+    System.out.println(Arrays.toString(n));
+  }
   public static int [] merge(int [] left, int[] right){
     int[] newArr = new int[left.length+right.length];
     int leftIndex = 0;
     int rightIndex = 0;
-    for (int i = 0; i<new.length; i++){
-      if (left[leftIndex] >= right[rightIndex]){
-        newArr[i] 
+    for (int i = 0; i<newArr.length; i++){
+      // if () {
+      //   newArr[i] = right[rightIndex];
+      //   rightIndex++;
+      // }
+      if (leftIndex < left.length && left[leftIndex] <= right[rightIndex]){
+        newArr[i] = left[leftIndex];
+        leftIndex++;
+      } else {
+        newArr[i] = right[rightIndex];
+        rightIndex++;
       }
     }
+    return newArr;
   }
 
-  public static void mergesort(data){
-    int [] temp  =  mergeSortH(data);
-    //copy temp into data
-  }
-
-  public static int[] mergesortH(data){
-    if (data.length > 1){
-       left = new int[data.length/2];
-       for (int i = 0; i<left.length; i++){
-         left[i] = data[i];
-       }
-       right = new int[data.length-data.length/2];
-       for (int i = 0; i<right.length; i++){
-         right[i] = data[i+data.length/2];
-       }
-       return merge(mergesortH(left),mergesortH(right))
-    }
-  }
+  // public static void mergesort(data){
+  //   int [] temp  =  mergeSortH(data);
+  //   //copy temp into data
+  // }
+  //
+  // public static int[] mergesortH(data){
+  //   if (data.length > 1){
+  //      left = new int[data.length/2];
+  //      for (int i = 0; i<left.length; i++){
+  //        left[i] = data[i];
+  //      }
+  //      right = new int[data.length-data.length/2];
+  //      for (int i = 0; i<right.length; i++){
+  //        right[i] = data[i+data.length/2];
+  //      }
+  //      return merge(mergesortH(left),mergesortH(right))
+  //   }
+  // }
 }
