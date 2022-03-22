3f1781ab54dd106864ce5178f0c63493341dff4d
Henry Bach <henrypbach@gmail.com>
2022-03-22 10:46:30 -0400

    mergeSort lst

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -13,7 +13,7 @@ public class Merge {
     int leftIndex = 0;
     int rightIndex = 0;
     for (int i = 0; i<newArr.length; i++){
-      if (leftIndex < left.length && left[leftIndex] <= right[rightIndex]){
+      if (rightIndex >= right.length || (leftIndex < left.length && left[leftIndex] <= right[rightIndex])){
         newArr[i] = left[leftIndex];
         leftIndex++;
       } else {
@@ -30,17 +30,16 @@ public class Merge {
   // }
 
   public static int[] mergesortH(int[] data){
-    if (data.length > 1){
-       int[] left = new int[data.length/2];
-       for (int i = 0; i<left.length; i++){
-         left[i] = data[i];
-       }
-       int[] right = new int[data.length-data.length/2];
-       for (int i = 0; i<right.length; i++){
-         right[i] = data[i+data.length/2];
-       }
-       return merge(mergesortH(left),mergesortH(right));
+    if (data.length < 2) return data;
+    int[] left = new int[data.length/2];
+    for (int i = 0; i<left.length; i++){
+      left[i] = data[i];
     }
-    return data;
+    int[] right = new int[data.length-data.length/2];
+    for (int i = 0; i<right.length; i++){
+      right[i] = data[i+data.length/2];
+    }
+    System.out.println(Arrays.toString(data)+"  "+Arrays.toString(left)+"  "+Arrays.toString(right));
+    return merge(mergesortH(left),mergesortH(right));
   }
 }
