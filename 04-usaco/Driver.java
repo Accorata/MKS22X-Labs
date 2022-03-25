2bb5cca693517e73c2b391eaa673d7c72bb8a94e
Henry Bach <henrypbach@gmail.com>
2022-03-24 22:10:00 -0400

    usaco tests

--- 04-usaco/Driver.java
+++ 04-usaco/Driver.java
@@ -1,8 +1,8 @@
 public class Driver {
   public static void main(String[] args){
-    long ans = Bronze.solve("makelake.1.in");
+    long ans = Bronze.solve("makelake.2.in");
     System.out.println(ans);
-    long ans2 = Silver.solve("ctravel.1.in");
+    long ans2 = Silver.solve("ctravel.2.in");
     System.out.println(ans2);
   }
 }
