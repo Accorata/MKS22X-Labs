d60ce13d867fc00c9404e86371543887a7aef94f
Henry Bach <henrypbach@gmail.com>
2022-03-28 10:41:29 -0400

    toStringDebug

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -14,14 +14,30 @@ public class MyDeque<E>{
   // public int size(){ }
   public String toString(){
     String ans = "[";
-    for (int i = 0; i<size; i++) {
+    for (int i = start; i<size; i++) {
       ans += data[i];
       if (i != size-1) ans += ", ";
     }
     return ans + "]";
   }
-  // public void addFirst(E element){ }
-  // public void addLast(E element){ }
+  public String toStringDebug(){
+    String ans = "[";
+    for (int i = 0; i<data.length; i++) {
+      ans += data[i];
+      if (i != data.length-1) ans += ", ";
+    }
+    return ans + "]\nStart: "+start+" End: "+end+" Size: "+size;
+  }
+  // public void addFirst(E element){
+  //   data[start-1] = element;
+  //   start--;
+  // }
+  public void addLast(E element){
+    data[end+1] = element;
+    size++;
+    end++;
+    if (end >= data.length) end = 0;
+  }
   // public E removeFirst(){ }
   // public E removeLast(){ }
   // public E getFirst(){ }
--- 07-mydeque/Test.java
+++ 07-mydeque/Test.java
@@ -1,6 +1,9 @@
 public class Test {
   public static void main(String[] args) {
-    MyDeque a = new MyDeque();
+    MyDeque<String> a = new MyDeque<String>();
+    System.out.println(a.toStringDebug());
+    a.addLast("test1");
+    System.out.println(a.toStringDebug());
     System.out.println(a);
   }
 }
