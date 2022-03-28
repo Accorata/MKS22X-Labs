00bee32b7bfbaa287ada8dc56dc90cbedb24e9f9
Henry Bach <henrypbach@gmail.com>
2022-03-28 10:48:03 -0400

    addFirst/addList 1st (-resize)

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -12,14 +12,14 @@ public class MyDeque<E>{
     data = d;
   }
   // public int size(){ }
-  public String toString(){
-    String ans = "[";
-    for (int i = start; i<size; i++) {
-      ans += data[i];
-      if (i != size-1) ans += ", ";
-    }
-    return ans + "]";
-  }
+  // public String toString(){
+  //   String ans = "[";
+  //   for (int i = start; i<size; i++) {
+  //     ans += data[i];
+  //     if (i != size-1) ans += ", ";
+  //   }
+  //   return ans + "]";
+  // }
   public String toStringDebug(){
     String ans = "[";
     for (int i = 0; i<data.length; i++) {
@@ -28,15 +28,17 @@ public class MyDeque<E>{
     }
     return ans + "]\nStart: "+start+" End: "+end+" Size: "+size;
   }
-  // public void addFirst(E element){
-  //   data[start-1] = element;
-  //   start--;
-  // }
+  public void addFirst(E element){
+    if (start <= 0) start = data.length;
+    data[start-1] = element;
+    size++;
+    start--;
+  }
   public void addLast(E element){
+    if (end >= data.length) end = -1;
     data[end+1] = element;
     size++;
     end++;
-    if (end >= data.length) end = 0;
   }
   // public E removeFirst(){ }
   // public E removeLast(){ }
--- 07-mydeque/Test.java
+++ 07-mydeque/Test.java
@@ -2,7 +2,9 @@ public class Test {
   public static void main(String[] args) {
     MyDeque<String> a = new MyDeque<String>();
     System.out.println(a.toStringDebug());
-    a.addLast("test1");
+    a.addFirst("test1");
+    a.addFirst("test2");
+    a.addFirst("test3");
     System.out.println(a.toStringDebug());
     System.out.println(a);
   }
