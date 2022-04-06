7d48c4dcf2481118a87bb88565e8c5292b34aacf
Henry Bach <henrypbach@gmail.com>
2022-04-06 10:23:01 -0400

    testing 100

--- 09-treeburning/BurnTests.java
+++ 09-treeburning/BurnTests.java
@@ -1,16 +1,19 @@
 public class BurnTests {
   public static void main(String[] args) {
-    System.out.println(average(100,0.5));
+    //System.out.println(average(100,0.5));
+    test(0.60,0.62,0.001);
   }
   public static double average (int size, double density){
     double ans = 0;
-    for (int i = 0; i<100; i++) {
+    for (int i = 0; i<1000; i++) {
       BurnTrees b = new BurnTrees(size,size,density);
       ans += b.run();
     }
-    return ans/100;
+    return ans/1000;
   }
-  public static void test(int start, int end, int interval){
-
+  public static void test(double start, double end, double interval){
+    for (double i = start; i<end; i+=interval){
+      System.out.println(i+": "+average(100,i)); //0.61
+    }
   }
 }
