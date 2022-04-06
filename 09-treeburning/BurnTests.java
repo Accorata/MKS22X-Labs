09f03413665b0f9781b3a06bf154085e00ccfe3c
Henry Bach <henrypbach@gmail.com>
2022-04-06 10:29:34 -0400

    looped testing

--- 09-treeburning/BurnTests.java
+++ 09-treeburning/BurnTests.java
@@ -1,7 +1,14 @@
+import java.util.Arrays;
 public class BurnTests {
   public static void main(String[] args) {
     //System.out.println(average(100,0.5));
-    test(0.60,0.62,0.001);
+    double start = 0;
+    double end = 1;
+    double interval = 0.1;
+    //for (int i=0; i<3; i++) {
+      System.out.println(Arrays.toString(test(start,end,interval)));
+    //}
+
   }
   public static double average (int size, double density){
     double ans = 0;
@@ -11,9 +18,21 @@ public class BurnTests {
     }
     return ans/1000;
   }
-  public static void test(double start, double end, double interval){
+  public static double[] test(double start, double end, double interval){
+    double[] max = new double[]{0,0,0,0};
     for (double i = start; i<end; i+=interval){
-      System.out.println(i+": "+average(100,i)); //0.61
+      double val = average(100,i);
+      if (val > max[0]) {
+        max[2] = max[0];
+        max[3] = max[1];
+        max[0] = val;
+        max[1] = i;
+      } else if (val > max[2]){
+        max[2] = val;
+        max[3] = i;
+      }
+      System.out.println(i+": "+val); //0.61
     }
+    return max;
   }
 }
